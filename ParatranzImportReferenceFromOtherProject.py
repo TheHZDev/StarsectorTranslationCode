a30c1afdbadc5f3913fd6d068c2336b7617c4fce
StarsectorTranslationCoder <34000406+TheHZDev@users.noreply.github.com>
2025-03-29 21:47:19 +0800

    250329更新
    +改进了术语复制逻辑，现在更加稳定

--- ParatranzImportReferenceFromOtherProject.py
+++ ParatranzImportReferenceFromOtherProject.py
@@ -1,11 +1,12 @@
-import requests
 from concurrent.futures import ThreadPoolExecutor
+
+import requests, json
 
 API_Tokens = ''  # 这是你在Paratranz的API Token
 rwAPI = 'https://paratranz.cn/api/projects/{0}/terms'
 inputProjectID = 3489  # 远行星号本体汉化项目的ID
 outputProjectID = []  # 其他需要依赖借鉴的远行星号mod项目的ID
-threadPool = ThreadPoolExecutor(max_workers=len(outputProjectID) * 25)
+threadPool = ThreadPoolExecutor(max_workers=1)
 
 HTTP_Header = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:120.0) Gecko/20100101 Firefox/120.0',
@@ -14,11 +15,15 @@ HTTP_Header = {
 }
 
 tInput = requests.get(rwAPI.format(inputProjectID), headers=HTTP_Header,
-                      params={'page': '1', 'pageSize': '1000'}).json()
+                      params={'page': '1', 'pageSize': '114514'}).json()
+
+termData = []
 for dictUnit in tInput['results']:
-    newTerm = {'term': dictUnit['term'], 'pos': dictUnit['pos'], 'note': dictUnit['note'],
-               'translation': dictUnit['translation'], 'variants': dictUnit['variants']}
-    for outID in outputProjectID:
-        threadPool.submit(requests.post, **dict(url=rwAPI.format(outID), headers=HTTP_Header, json=newTerm))
+    termData.append({'term': dictUnit['term'], 'pos': dictUnit['pos'], 'note': dictUnit['note'],
+                     'translation': dictUnit['translation'], 'variants': dictUnit['variants']})
+
+for outID in outputProjectID:
+    threadPool.submit(requests.put, **dict(url=rwAPI.format(outID), headers=HTTP_Header,
+                                           files={'file': ('1.json', json.dumps(termData))}))
 
 threadPool.shutdown()
